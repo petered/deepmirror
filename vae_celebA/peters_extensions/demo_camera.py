54ae3a5019629966d5c1dc48873c1c0091ad3263
Peter O'Connor <peter.ed.oconnor@gmail.com>
2019-01-15 18:58:15 +0100

    ok

--- vae_celebA/peters_extensions/demo_camera.py
+++ vae_celebA/peters_extensions/demo_camera.py
@@ -3,14 +3,14 @@
 if __name__ == '__main__':
     import itertools
     from vae_celebA.image_utils.video_camera import VideoCamera
-    from artemis.plotting.db_plotting import dbplot, hold_dbplots
     import cv2
 
-    cam = VideoCamera(size=(640, 480), device=0, mode='rgb')
+    cam = VideoCamera(size=(640, 480), device=0, mode='bgr')
 
     for t in itertools.count(0):
         im = cam.read()
         if im is not None:
-            dbplot(im, 'im')
+            cv2.imshow('im', im)
+            cv2.waitKey(1)
         else:
             print('No Camera Image')
